9a573cc1fcc7946eb69079f3287bd0544d0528e3
hoangkhp <116267046+hoangkhp@users.noreply.github.com>
2024-10-09 15:45:27 +0700

    create Router for project

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import ProductPage from './pages/product.jsx';
+import UserPage from './pages/user.jsx';
+import './styles/global.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/users",
+    element: <UserPage />,
+  },
+  {
+    path: "/product",
+    element: <ProductPage />,
+  },
+]);
+<RouterProvider router={router} />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} /> {/* Sửa chỗ này */}
   </React.StrictMode>,
 )
